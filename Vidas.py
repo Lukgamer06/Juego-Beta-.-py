67b66f2ee1df79d4bf23b8fae71aa5559a04ec15
Lukgamer06 <166557319+Lukgamer06@users.noreply.github.com>
2024-10-15 15:41:20 -0500

    Vidas.py

---
+++ Vidas.py
@@ -0,0 +1,82 @@
+import pygame
+import random
+
+class Vidas_izq:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.ancho = 50
+        self.alto = 50
+        self.velocidad = 25
+        self.color = "black"
+        self.rect = pygame.Rect (self.x , self.y , self.ancho , self.alto)
+        self.imagen = pygame.image.load("Python/Juegos/PYgame/Imagenes/Vida.png")
+        self.tamano = pygame.transform.scale(self.imagen,(self.ancho,self.alto))
+    
+    def dibujar(self , ventana):
+        self.rect = pygame.Rect (self.x , self.y , self.ancho , self.alto)
+        pygame.draw.rect(ventana , self.color , self.rect)
+        ventana.blit(self.tamano,(self.x,self.y))
+
+    def movimiento(self):
+        self.x += self.velocidad
+
+class Vidas_der:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.ancho = 50
+        self.alto = 50
+        self.velocidad = 25
+        self.color = "black"
+        self.rect = pygame.Rect (self.x , self.y , self.ancho , self.alto)
+        self.imagen = pygame.image.load("Python/Juegos/PYgame/Imagenes/Vida.png")
+        self.tamano = pygame.transform.scale(self.imagen,(self.ancho,self.alto))
+
+    def dibujar(self , ventana):
+        self.rect = pygame.Rect (self.x , self.y , self.ancho , self.alto)
+        pygame.draw.rect(ventana , self.color , self.rect)
+        ventana.blit(self.tamano,(self.x,self.y))
+
+    def movimiento(self):
+        self.x -= self.velocidad
+
+class Vidas_Up:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.ancho = 50
+        self.alto = 50
+        self.velocidad = 25
+        self.color = "black"
+        self.rect = pygame.Rect (self.x , self.y , self.ancho , self.alto)
+        self.imagen = pygame.image.load("Python/Juegos/PYgame/Imagenes/Vida.png")
+        self.tamano = pygame.transform.scale(self.imagen,(self.ancho,self.alto))
+
+    def dibujar(self , ventana):
+        self.rect = pygame.Rect (self.x , self.y , self.ancho , self.alto)
+        pygame.draw.rect(ventana , self.color , self.rect)
+        ventana.blit(self.tamano,(self.x,self.y))
+
+    def movimiento(self):
+        self.y += self.velocidad
+
+class Vidas_Down:
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+        self.ancho = 50
+        self.alto = 50
+        self.velocidad = 25
+        self.color = "black"
+        self.rect = pygame.Rect (self.x , self.y , self.ancho , self.alto)
+        self.imagen = pygame.image.load("Python/Juegos/PYgame/Imagenes/Vida.png")
+        self.tamano = pygame.transform.scale(self.imagen,(self.ancho,self.alto))
+
+    def dibujar(self , ventana):
+        self.rect = pygame.Rect (self.x , self.y , self.ancho , self.alto)
+        pygame.draw.rect(ventana , self.color , self.rect)
+        ventana.blit(self.tamano,(self.x,self.y))
+
+    def movimiento(self):
+        self.y -= self.velocidad
